62bba6b03ee3741761943066df212a9c955d675d
leo-barbosa-oliveira <barbosa.olivera09@gmail.com>
2025-04-04 02:08:08 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "zhqh77",
   viewportHeight: 880, //altera a altura
   viewportWidth: 1280, // altera a larguda
   e2e: {}, //indica que é um projeto end2end
